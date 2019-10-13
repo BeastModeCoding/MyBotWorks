a7d02b7f223976baddad66a78b57fe6bb13bede6
BeastModeCoding <56511366+BeastModeCoding@users.noreply.github.com>
2019-10-13 20:44:27 +0300

    Without syn.txt

--- bot.py
+++ bot.py
@@ -12,12 +12,12 @@ print("Step 2. Connecting")
 
 morph = MorphAnalyzer()    # Обработка слов на русском языке
 alf = list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя ")
-dct = open("syn.txt", "r", encoding="utf-8").read().split("\n")
-bad_words = open("bad_words.txt", "r", encoding="utf-8").read().split("\n")
-frmt_dct = []
-for txt in dct:                         # ???
-    frmt_dct.append(txt.split("|"))     # ???
-del dct
+# dct = open("syn.txt", "r", encoding="utf-8").read().split("\n")
+# bad_words = open("bad_words.txt", "r", encoding="utf-8").read().split("\n")
+# frmt_dct = []
+# for txt in dct:                         # ???
+#     frmt_dct.append(txt.split("|"))     # ???
+# del dct
 print("Dictionary has been analyzed")
 run_for_all = True
 run_for_admin = False
