c4e6a108091307ca65bef223e9346dfe9c8352e6
BeastModeCoding <56511366+BeastModeCoding@users.noreply.github.com>
2019-10-13 20:40:07 +0300

    Create RoleTree.py

---
+++ RoleTree.py
@@ -0,0 +1,54 @@
+# TO_DO
+# Вставить все ссылки, проверить их на работоспособность
+
+# Студенты
+Students = {
+	"Питание": "https://vk.com/away.php?utf=1&to=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dru.mihailpro.mpeischedule",
+	"Общежитие": {
+		"Общежития и адреса": "http://campusmpei.ru - ссылка на сайт общежитий МЭИ (адресса общежитий и контакты комендатов)",
+		"Правила проживания": "http://campusmpei.ru/prozhivanie.html",
+		"Соц.объекты(санаторий/профилакторий МЭИ)": "http://campusmpei.ru/o-gorodke/sotsialnye-ob-ekty.html",
+		"Гид по студенческому городку МЭИ": "http://campusmpei.ru/zhizn-v-gorodke/gid-po-gorodku.html",
+		"Стоимость проживания": "http://campusmpei.ru/images/Documents/prikaz%20ob%20oplate%20prozhivanija.pdf"
+	},
+	"Учебная деятельность": {
+		"Система БАРС": "https://mpei.ru/Structure/uchchast/icc/ddiisas/Pages/bars.aspx",
+		"Расписание": "График учебного процесса - https://vk.com/away.php?utf=1&to=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dru.mihailpro.mpeischedule",
+		"Сессия":{
+			"График учебного процесса": "https://vk.com/away.php?utf=1&to=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dru.mihailpro.mpeischedule",
+			"Критерии оценивания": "https://mpei.ru/Education/StudyProcess/Documents/ins-12-23.pdf",
+			"Промежуточная аттестация": "https://mpei.ru/AboutUniverse/OficialInfo/Orders2018/MPEI-18-831.pdf"
+		}
+	},
+	"Социальные выплаты": {
+		"Стипендия": {
+			"График выплат": "https://mpei.ru/Structure/fech/accountingmanagement/sd/Pages/StipGraph.aspx",
+			"Другие формы материальной поддержки студентов": "Положение о стипендиальном обеспечении и других формах материальной поддержки учащихся - https://mpei.ru/Education/StudyProcess/Olympics/Documents/Position_2014.pdf"
+		},
+		"Материальная помощь": {
+			"Категории студентов для мат. помощи": "http://profcom.mpei.ru/assets/docs/Categories_of_students.docx",
+			"Информация и бланк заполнения": "http://profcom.mpei.ru/msupport.html"
+		},
+		"Дотация": "Категории студентов, бланки для заполнения, размер выплат - http://profcom.mpei.ru/subsidies.html",
+		"Единовременные выплаты": {
+			"Категории студентов": "http://profcom.mpei.ru/assets/docs/Categories_of_students.docx",
+			"Информация и бланк заполнения": "http://profcom.mpei.ru/payments.html"
+		}
+	},
+	"Студенческие организации": "https://vk.com/doc10238270_515008802?hash=2d04ad1d1403511e6f&dl=941501f98e3e83b2e3",
+	"Мероприятия": "https://mpei.ru/news/Lists/developments/calendar.aspx"
+}
+
+# Преподаватели
+Teachers = {
+	"Краткая информация о персоналии": "https://mpei.ru/personal/Pages/default.aspx",
+	"Расписание преподавателей": "Вы свободны!"
+}
+
+# Дирекция
+Staff = {
+	"Документация": "https://mpei.ru/AboutUniverse/OficialInfo/Pages/Orders.aspx",
+	"Учебный журнал": "Мы его потеряли :(",
+	"Руководство": "https://mpei.ru/Structure/managementpersonnel/rectorat/Pages/default.aspx",
+	"Совет директоров": "https://mpei.ru/Structure/managementpersonnel/board_of_directors/Pages/default.aspx"
+}
